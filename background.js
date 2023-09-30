8e76a0984edf2637b10952b7a3cbcf048e08839f
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2023-09-30 12:33:53 -0400

    feat: text replacement hotkey added

--- background.js
+++ background.js
@@ -6,9 +6,19 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener(async (_info, tab) => {
+  triggerReplacePhrasesAction(tab);
+});
+
+chrome.commands.onCommand.addListener(async (command, tab) => {
+  if (command === "replace-phrases") {
+    triggerReplacePhrasesAction(tab);
+  }
+});
+
+async function triggerReplacePhrasesAction(tab) {
   await chrome.storage.sync.set({
     currentURL: tab.url,
   });
 
   chrome.tabs.sendMessage(tab.id, { action: "replacePhrases" });
-});
+}
